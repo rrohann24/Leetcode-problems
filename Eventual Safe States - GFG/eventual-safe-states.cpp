c734d9d32c03979709e4364ac5736d1a3e994fa1
rrohann24 <111134616+rrohann24@users.noreply.github.com>
2023-04-07 13:10:20 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.cpp
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -10,35 +10,59 @@ using namespace std;
 
 class Solution {
   public:
-  bool check(int node, vector<int> adj[],vector<int> &vis){
+  bool dfs(int node, vector<int> &vis,vector<int> adj[]){
         vis[node]=1;
-        //curdfs[node]=1;
         vis[node]++;
-        for(int i=0; i<adj[node].size();i++){
-            if(!vis[adj[node][i]]){
-                if(check(adj[node][i],adj,vis)) return true;
+        
+        for(auto it: adj[node]){
+            if(vis[it]==2) return true;
+            
+            else if(vis[it]==0){
+                if(dfs(it,vis,adj)) return true;
             }
-            else if(vis[adj[node][i]]==2) return true;
         }
-        //curdfs[node] = 0;
-        vis[node]=1;
+        
+        vis[node]--;
         return false;
+        
     }
     vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
         // code here
-        vector<int> vis(V,0);
-        //vector<bool> curdfs(V,0);
+        /*vector<int> vis(V,0);
+        vector<int> safe(V,1);
+        
         for(int i=0; i<V; i++){
             if(!vis[i]){
-                check(i,adj,vis);
+             bool check = dfs(i,vis,adj);
             }
         }
         vector<int> ans;
+        for(int i=0; i<V; i++) if(vis[i]==1) ans.push_back(i);
+        
+        return ans;*/
+        vector<int> ans;
+        vector<int> indeg(V);
+        vector<int> revAdj[V];
         for(int i=0; i<V; i++){
-            if(vis[i]==1){
-                ans.push_back(i);
+            for(auto it: adj[i]){
+                revAdj[it].push_back(i);
+                indeg[i]++;
             }
         }
+        queue<int> q;
+        for(int i=0; i<V ;i++){
+            if(indeg[i]==0) q.push(i);
+        }
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it: revAdj[node]){
+                indeg[it]--;
+                if(indeg[it]==0) q.push(it);
+            }
+        }
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
